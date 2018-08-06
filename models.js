c9f6ccafeb4db13d6b6527b6755228a3e3d45930
Daniel Casasola <casasolad86@gmail.com>
2018-08-06 16:10:11 -0500

    created models

--- models.js
+++ models.js
@@ -1,3 +1,46 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
 module.exports = db;
+
+const Gardener = db.define('gardener', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    age:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+})
+const Plot = db.define('plot', {
+    size:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    shaded:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+
+    }
+
+})
+const Vegetable = db.define('vegetable', {
+    name:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    color:{
+        type: Sequelize.STRING,
+        allowNull: false
+
+    },
+    planted_on:{
+        type: Sequelize.DATE,
+        allowNull: false
+
+    }
+
+})
+
+Vegetable.belongsToMany(Plot, {through: 'friendship'})
+Plot.belongsToMany(Vegetable, {through: 'friendship'})
